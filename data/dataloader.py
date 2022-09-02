a3f66c254d39de5b94832dba8118191de5071d7d
momoyama <kanaami1@gmail.com>
2022-09-02 16:06:24 +0900

    add predict_datloader method to MyDataModule

--- data/dataloader.py
+++ data/dataloader.py
@@ -11,6 +11,7 @@ import pytorch_lightning as pl
 from utils.timefeatures import time_features
 from sklearn.preprocessing import StandardScaler
 #from utils.tools import StandardScaler
+from utils.tools import reduce_memory_usage
 from utils.configure import Config
 
 
@@ -35,6 +36,7 @@ class MyDataset(Dataset):
     def __read_data__(self):
         self.scaler = StandardScaler()
         df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
+        df_raw = reduce_memory_usage(df_raw)
         if self.cols:
             cols = self.cols.copy()
             cols.remove(self.target)
@@ -112,6 +114,75 @@ class MyDataset(Dataset):
         return self.scaler.inverse_transform(data)
 
 
+class MyDataset_Pred(Dataset):
+    def __init__(self, root_path='data', flag='pred',
+                 data_path='test.csv',
+                 target='inflow', scale=True, inverse=False,
+                 timeenc=0, freq='h', cols=None):
+        assert flag in ['pred']
+        self.target = target
+        self.scale = scale
+        self.inverse = inverse
+        self.timeenc = timeenc
+        self.freq = freq
+        self.cols = cols
+        self.root_path = root_path
+        self.data_path = data_path
+        self.__read_data__()
+
+    def __read_data__(self):
+        self.scaler = MyDataset().scaler
+        df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
+        df_raw = reduce_memory_usage(df_raw)
+        if self.cols:
+            cols = self.cols.copy()
+            cols.remove(self.target)
+        else:
+            cols = list(df_raw.columns)
+            cols.remove(self.target)
+            cols.remove('date')
+        df_raw = df_raw[['date'] + cols + [self.target]]
+
+        if 'id' in df_raw.columns:
+            df_raw.drop('id', axis=1, inplace=True)
+
+        cols_data = df_raw.columns[1:]
+        df_data = df_raw[cols_data]
+
+        if self.scale:
+            data = self.scaler.transform(df_data.to_numpy())
+        else:
+            data = df_data.to_numpy()
+
+        tmp_stamp = df_raw[['date']]
+        tmp_stamp['date'] = pd.to_datetime(tmp_stamp.date)
+        df_stamp = pd.DataFrame(columns=['date'])
+        df_stamp.date = list(tmp_stamp.date.to_numpy())
+        data_stamp = time_features(df_stamp, timeenc=self.timeenc, freq=self.freq[-1:])
+
+        self.data_x = data[:, :-1].astype(np.float32)
+        if self.inverse:
+            self.data_y = df_data.to_numpy()[:, -1].astype(np.float32)
+        else:
+            self.data_y = data[:, -1].astype(np.float32)
+        self.data_stamp = data_stamp
+
+    def __getitem__(self, index):
+        seq_x = self.data_x[index]
+        if self.inverse:
+            seq_y = np.concatenate([self.data_x[index], self.data_y[index]], 0)
+        else:
+            seq_y = self.data_y[index]
+        seq_mark = self.data_stamp[index]
+        return seq_x, seq_y, seq_mark
+
+    def __len__(self):
+        return len(self.data_x)
+
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data)
+
+
 class MyDataModule(pl.LightningDataModule):
     def __init__(self, config: Config):
         super(MyDataModule, self).__init__()
@@ -120,9 +191,13 @@ class MyDataModule(pl.LightningDataModule):
         self.pin_memory = config.pin_memory
 
     def setup(self, stage: Optional[str] = None):
-        self.dataset_train = MyDataset(flag='train')
-        self.dataset_val = MyDataset(flag='val')
-        self.dataset_test = MyDataset(flag='test')
+        if stage == 'fit' or stage is None:
+            self.dataset_train = MyDataset(flag='train')
+            self.dataset_val = MyDataset(flag='val')
+        if stage == 'test' or stage is None:
+            self.dataset_test = MyDataset(flag='test')
+        if stage == 'predict' or stage is None:
+            self.dataset_pred = MyDataset_Pred(flag='pred')
 
     def train_dataloader(self):
         train_loader = DataLoader(
@@ -156,3 +231,14 @@ class MyDataModule(pl.LightningDataModule):
             drop_last=True
         )
         return test_loader
+
+    def predict_dataloader(self):
+        pred_loader = DataLoader(
+            self.dataset_pred,
+            batch_size=1,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            drop_last=False
+        )
+        return pred_loader
